851444ea9f1f84cb153d7eb167acbd4cdfe0c671
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-06 16:18:35 -0700

    Add documentation with jsdoc to all methods, functions, and classes

--- src/middleware/404.js
+++ src/middleware/404.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+   * @module 404
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @desc server error handler
+   */
 module.exports = (req,res) => {
   let error = { error: 'Resource Not Found' };
   res.status(404).json(error).end();
